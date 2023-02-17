22a853c13b261558d0f591189d3ed25041d0de10
rater31 <fedehidalgo89@gmail.com>
2023-02-17 11:38:05 -0300

    net
    se agrego carpetas y archivos js, css, y html ya todos linkeados

---
+++ opcion2/opcion2/opcion2.js
@@ -0,0 +1,21 @@
+// barras
+let barra1=document.querySelector("#barra1");
+let barra2=document.querySelector("#barra2");
+let barra3=document.querySelector("#barra3");
+// menu desplegable
+let menuDesplegable=document.querySelector("#menu");
+
+
+barra2.addEventListener("click", animacion);
+barra3.addEventListener("click", animacion);
+
+
+
+function animacion(){
+    barra1.classList.toggle('barra1');
+    barra2.classList.toggle('barra2');
+    barra3.classList.toggle('barra3');
+
+    menuDesplegable.classList.toggle('animacion');
+    console.log('Entro ');
+}
